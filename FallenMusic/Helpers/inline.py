a7f29a37dbfa8cdceb701ee5bf00445da9c8bade
Akshit <120463129+sukkunmusic@users.noreply.github.com>
2023-04-03 15:20:59 +0530

    Update inline.py
    Signed-off-by: Akshit <120463129+sukkunmusic@users.noreply.github.com>

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -56,7 +56,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/AnonymousX1025/FallenMusic"
+            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://te.legra.ph/file/1a73e2d2834b5778106a5.mp4"
         ),
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
     ],
@@ -76,7 +76,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/TheAnonymous2005/FallenMusic"
+            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://te.legra.ph/file/1a73e2d2834b5778106a5.mp4"
         ),
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
     ],
