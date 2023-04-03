ebdc2893e4258851782f3bb57b521313b8a0dc39
Akshit <120463129+sukkunmusic@users.noreply.github.com>
2023-04-03 15:10:07 +0530

    Update ping.py
    Signed-off-by: Akshit <120463129+sukkunmusic@users.noreply.github.com>

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -59,8 +59,8 @@ async def ping_fallen(_, message: Message):
                 [
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
-                        "✨ sᴏᴜʀᴄᴇ ✨",
-                        url="https://github.com/AnonymousX1025/FallenMusic",
+                        "✨ ᴜᴘᴅᴀᴛᴇs ✨",
+                        url="https://t.me/TheBotsUpdate",
                     ),
                 ],
             ]
