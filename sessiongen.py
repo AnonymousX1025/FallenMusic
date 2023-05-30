da8af64c85f41112344e40ff102fc9d5fb87884f
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-05-31 00:45:49 +0530

    Create sessiongen.py
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>

---
+++ sessiongen.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from pyrogram import Client
+
+
+API_ID = input("\nEnter Your API ID :\n > ")
+API_HASH = input("\nEnter Your API HASH :\n > ")
+
+print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
+
+fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+
+
+async def main():
+    await fallen.start()
+    sess = await fallen.export_session_string()
+    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @FallenAssociation"
+    ok = await fallen.send_message("me", txt)
+    print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
+
+
+asyncio.run(main())
