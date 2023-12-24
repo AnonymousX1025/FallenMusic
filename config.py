141ea8dcabe5d72d68994cfcc66d80f6d08580e1
Wildan018 <152429993+Wildan018@users.noreply.github.com>
2023-12-25 01:46:53 +0700

    Update and rename config.py to config.py
    Music great bot 
    
    Signed-off-by: Wildan018 <152429993+Wildan018@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "29535970"))
+API_HASH = getenv("API_HASH", "bead7e32f0dcfc317b0b57717d819a14")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6528153674:AAH2GNkbSdEcbt41goGBXDUQtZD4GrlUjRo")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "5779185981"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://i.postimg.cc/Pqy82pxN/a8417f70-4e09-473b-b8ac-521989f048c5.jpg")
+START_IMG = getenv("START_IMG", "https://i.postimg.cc/Pqy82pxN/a8417f70-4e09-473b-b8ac-521989f048c5.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", "BQHCruIApls9NvanUjG87IBujQrLyZ37ESXcuAmIKeoRAdm9iHqzN-Dsd9RoTl1yZmw8zADGxUPARt_bMqUiRkHaUKi-uHNiDWWrQPYRQk_Lba0uPR85_Q38WV-cpvPKkAAab2TpkE-SWpswOJv2u_uXJQpP4WrXBHGSf_vr72xHHMchDudicC4bunw2RnQgyWW89s3cSaXc9xRVqBfvOerYAHgg6nVetKu7duYqojmJTye6YgKiEUoBYK4Y0o1CCV78v4cGEVGT-620nleth9f0ZLVeOExvTVkWUxELMv1WAZRPP5ZkIjvDkl22_o7ka34axuARH9aWUucm-cJt6kqKUkfWngAAAABXh0liAA")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/musicgreat_bott")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Great0623")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://i.postimg.cc/Pqy82pxN/a8417f70-4e09-473b-b8ac-521989f048c5.jpg"
