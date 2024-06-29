40d29255f1aaff26495ed15e82cfb210c1adbd16
ramowlf <ramodiscord26@gmail.com>
2024-06-29 22:41:35 +0300

    Update config.py
    Signed-off-by: ramowlf <ramodiscord26@gmail.com>

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TeamL7X")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BotAltyapi")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
