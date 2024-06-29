b0598e95a520cfc36bfec9614c4ba4f6d2a9bdd5
ramowlf <ramodiscord26@gmail.com>
2024-06-30 00:41:08 +0300

    Update pause.py
    Signed-off-by: ramowlf <ramodiscord26@gmail.com>

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ʀᴇsᴜᴍᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
+            "Hatırladın mı, yayını devam ettirdin mi ?"
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"➻ sᴛʀᴇᴀᴍ ᴩᴀᴜsᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"➻ Yayın durduruldu 🥺\n│ \nTarafından : {message.from_user.mention} 🥀",
         reply_markup=close_key,
     )
