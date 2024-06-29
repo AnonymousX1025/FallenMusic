ddf91684eb253ed8bbd7a04bce68dce524ca92e8
ramowlf <ramodiscord26@gmail.com>
2024-06-30 01:55:45 +0300

    Update search.py
    Signed-off-by: ramowlf <ramodiscord26@gmail.com>

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -27,7 +27,7 @@ from youtube_search import YoutubeSearch
 from FallenMusic import app
 
 
-@app.on_message(filters.command(["search"]))
+@app.on_message(filters.command(["arama"]))
 async def ytsearch(_, message: Message):
     try:
         await message.delete()
@@ -35,24 +35,24 @@ async def ytsearch(_, message: Message):
         pass
     try:
         if len(message.command) < 2:
-            return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ !")
+            return await message.reply_text("» Arama yapmak istediğin metini gir bebeğim !")
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("🔎")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"✨ ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : `{results[i]['duration']}`\n"
-            text += f"👀 ᴠɪᴇᴡs : `{results[i]['views']}`\n"
-            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
-            text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"✨ Başlık : {results[i]['title']}\n"
+            text += f"⏱ Süre : `{results[i]['duration']}`\n"
+            text += f"👀 İzlenme : `{results[i]['views']}`\n"
+            text += f"📣 Kanal : {results[i]['channel']}\n"
+            text += f"🔗 Link : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         key = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="ᴄʟᴏsᴇ",
+                        text="Geri",
                         callback_data=f"forceclose abc|{message.from_user.id}",
                     ),
                 ]
