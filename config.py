4dd8b7c6b91c5102e59380d40107cfac35ae6255
ramowlf <ramodiscord26@gmail.com>
2024-06-30 03:10:15 +0300

    Update config.py
    Signed-off-by: ramowlf <ramodiscord26@gmail.com>

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/366e6e941b048e36b8468.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/a39509bf85f227739bc68.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BotAltyapi")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://te.legra.ph/file/366e6e941b048e36b8468.jpg"
