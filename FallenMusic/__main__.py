7fcc5ef53ca5c6c243efb474001279f138d2b5dc
ramowlf <ramodiscord26@gmail.com>
2024-06-30 03:23:37 +0300

    Update __main__.py
    Signed-off-by: ramowlf <ramodiscord26@gmail.com>

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -58,7 +58,7 @@ async def fallen_startup():
     try:
         await app.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+            f"✯ BOT ALTYAPI MUSİC ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
